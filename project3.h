b07b20ac3dfd369a6e02efdbf22c6e72abe961ed
jeremyverde <jtgreen@cs.colostate.edu>
2017-11-27 09:00:06 -0700

    Skeleton of manager, starts N "router" processes. Reads from file.

--- project3.h
+++ project3.h
@@ -1,8 +1,19 @@
-//
-// Created by jeremy on 11/26/17.
-//
-
 #ifndef PROJECT3_PROJECT3_H
 #define PROJECT3_PROJECT3_H
 
+#include <fstream>
+
+using namespace std;
+
+class router {
+public:
+    int startRouter(ofstream &o);
+};
+
+class manager {
+public:
+    static int usage();
+
+    const char *killFile = "kill.txt";
+};
 #endif //PROJECT3_PROJECT3_H
