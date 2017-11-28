61ce65fbe65b5d952b981a7f43b711379e06f68a
jeremyverde <jtgreen@cs.colostate.edu>
2017-11-28 00:13:48 -0700

    Does some basic TCP stuff now

--- project3.h
+++ project3.h
@@ -2,12 +2,29 @@
 #define PROJECT3_PROJECT3_H
 
 #include <fstream>
+#include <iostream>
+#include <zconf.h>
+#include <wait.h>
+#include <sstream>
+#include <cstring>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <cassert>
+
 
 using namespace std;
 
 class router {
 public:
-    int startRouter(ofstream &o);
+    int startRouter(ofstream &o, const char *port);
+
+    void GetPrimaryIp(char *buffer, size_t buflen);
+
+    void *get_in_addr(struct sockaddr *sa);
+
+#define MAXDATASIZE 100 // max number of bytes we can get at once
+    bool debug = true;
 };
 
 class manager {
@@ -17,5 +34,8 @@ public:
     static int manage(ofstream &ostr);
 
     const char *killFile = "kill.txt";
+
+#define PORT "9001"  // the port users will be connecting to
+#define BACKLOG 10     // how many pending connections queue will hold
 };
 #endif //PROJECT3_PROJECT3_H
