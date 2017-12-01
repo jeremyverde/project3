e91ea8f4f5595450d0b453effac7841abc7b8b8e
jeremyverde <jtgreen@cs.colostate.edu>
2017-11-30 20:29:56 -0700

    UDP finally works. Now to actually do some routing.

--- router.cpp
+++ router.cpp
@@ -2,23 +2,9 @@
 
 using namespace std;
 
-void router::getTime(char *stamp, int len) {
-    memset(stamp, 0, sizeof(stamp));
-    char timeBuf[len];
-    string tString;
-    struct timeval tVal{};
-    gettimeofday(&tVal, nullptr);
-    time_t now = tVal.tv_sec;
-    if (strftime(timeBuf, sizeof(timeBuf), "[%F %X.", localtime(&now))) {
-        //cout << timeBuf << endl;
-    } else {
-        cerr << "Time's broken" << endl;
-    }
-    tString = timeBuf;
-    tString.append(to_string(tVal.tv_usec) + "]");
-    for (int i = 0; i < len; i++) {
-        stamp[i] = tString[i];
-    }
+router::router() {
+    links = vector<manager::link>(11);
+    nLinks = 0;
 }
 
 // get sockaddr, IPv4 or IPv6:
@@ -69,7 +55,7 @@ void router::udpListen(ofstream &o, string &portnum) {
         portnum = port;
         break;
     }
-
+/*
     if (p == nullptr) {
         fprintf(stderr, "listener: failed to bind socket\n");
         exit(2);
@@ -78,7 +64,7 @@ void router::udpListen(ofstream &o, string &portnum) {
     freeaddrinfo(servinfo);
 
     addr_len = sizeof their_addr;
-    /*if ((numbytes = recvfrom(sockfd, buf, MAXDATASIZE - 1, 0,
+    if ((numbytes = recvfrom(sockfd, buf, MAXDATASIZE - 1, 0,
                              (struct sockaddr *) &their_addr, &addr_len)) == -1) {
         perror("recvfrom");
         exit(1);
@@ -86,17 +72,19 @@ void router::udpListen(ofstream &o, string &portnum) {
 
     printf("listener: got packet from %s\n",
            inet_ntop(their_addr.ss_family,
-                     get_in_addr((struct sockaddr *) &their_addr),
+                     manager::get_in_addr((struct sockaddr *) &their_addr),
                      s, sizeof s));
     printf("listener: packet is %zi bytes long\n", numbytes);
     buf[numbytes] = '\0';
     printf("listener: packet contains \"%s\"\n", buf);
-*/
+
     //close(sockfd);
+    */
 }
 
 // based on example at: https://stackoverflow.com/questions/212528/get-the-ip-address-of-the-machine
 void router::GetPrimaryIp(char *buffer, size_t buflen) {
+
     assert(buflen >= 16);
 
     int sock = socket(AF_INET, SOCK_DGRAM, 0);
@@ -126,28 +114,62 @@ void router::GetPrimaryIp(char *buffer, size_t buflen) {
 
 string router::fillPacket(string &c, string &id) {
     string packet = move(c);
+    packet.append("|");
     packet.append(id);
     return packet;
 }
 
 void router::writeHeader(ofstream &o) {
     char stamp[100];
-    getTime(stamp, sizeof(stamp));
+    manager::getTime(stamp, sizeof(stamp));
     o << "Router: " << getpid() << " Log File" << endl;
     o << "Log File Created: " << stamp << endl;
 }
 
 int router::startRouter(ofstream &ostr) {
     int mypid = getpid();
+    // UDP SECTION
+    int sockfd = 0;
+    struct addrinfo udphints{}, *servinfo, *p;
+    int rv;
+    ssize_t numbytes;
+    struct sockaddr_storage their_addr{};
+    char udpbuf[MAXDATASIZE];
+    socklen_t addr_len;
+    char s[INET6_ADDRSTRLEN];
+    GetPrimaryIp(udpbuf, sizeof(udpbuf));
+    string port;
+
+    memset(&udphints, 0, sizeof udphints);
+    udphints.ai_family = AF_UNSPEC; // set to AF_INET to force IPv4
+    udphints.ai_socktype = SOCK_DGRAM;
+    udphints.ai_flags = AI_PASSIVE; // use my IP
+    port = to_string((mypid % 6997) + 1100);
+
+    if ((rv = getaddrinfo(nullptr, port.c_str(), &udphints, &servinfo)) != 0) {
+        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+        exit(1);
+    }
+
+    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) == -1) {
+        perror("Router: socket");
+    }
+    if (bind(sockfd, servinfo->ai_addr, servinfo->ai_addrlen) == -1) {
+        close(sockfd);
+        perror("Router: bind");
+    }
+
+    // TCP SECTION
     string pid = to_string(mypid);
     string file = "router";
     file.append(pid + ".out");
     ofstream out(file);
     writeHeader(out);
     char stamp[100];
-    getTime(stamp, sizeof(stamp));
+    manager::getTime(stamp, sizeof(stamp));
     out << stamp << "[Router: " << mypid << "] writing pid to kill file." << endl;
     ostr << mypid << endl;
+    out << endl << "[Router] my port is: " << port << endl << endl;
 
     // code based largely off of beej's guide example program, see README
     int sok = 0;
@@ -170,16 +192,16 @@ int router::startRouter(ofstream &ostr) {
     string packet;
 
     // start up UDP socket
-    udpListen(out, portnum);
+    //udpListen(out, portnum);
 
     // make sure the manager spins up first
-    sleep(1);
+    //sleep(1);
 
     memset(&hints, 0, sizeof hints); // make sure the struct is empty
     hints.ai_family = AF_UNSPEC;     // don't care IPv4 or IPv6
     hints.ai_socktype = SOCK_STREAM; // TCP stream sockets
     hints.ai_flags = AI_PASSIVE;     // fill in my IP for me
-    getTime(stamp, sizeof(stamp));
+    manager::getTime(stamp, sizeof(stamp));
     out << stamp << "[Router: " << mypid << "] connecting to IP: " << buf << " Port: " << PORT << endl;
     if ((status = getaddrinfo(IP, PORT, &hints, &ai)) != 0) {
         fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(status));
@@ -196,7 +218,7 @@ int router::startRouter(ofstream &ostr) {
         cerr << "[Router] connection failed" << endl;
         return -1;
     } else {
-        getTime(stamp, sizeof(stamp));
+        manager::getTime(stamp, sizeof(stamp));
         out << stamp << "[Router: " << mypid << "] Connected." << endl;
     }
     // Test if the socket is in non-blocking mode:
@@ -209,7 +231,7 @@ int router::startRouter(ofstream &ostr) {
 
     freeaddrinfo(ai);
 
-    string b, in, id;
+    string b, in, id, adj;
     while (true) {
         if (ready) {
             memset(&buf, 0, sizeof(buf));
@@ -218,7 +240,7 @@ int router::startRouter(ofstream &ostr) {
             for (int k = 0; k < b.length(); k++) {
                 buf[k] = b[k];
             }
-            getTime(stamp, sizeof(stamp));
+            manager::getTime(stamp, sizeof(stamp));
             out << stamp << "[Router: " << mypid << "] sending: " << buf << endl;
             if (send(sok, buf, sizeof(buf), 0) == -1) {
                 perror("[Router: ] send failed");
@@ -234,7 +256,7 @@ int router::startRouter(ofstream &ostr) {
             for (int k = 0; k < b.length(); k++) {
                 buf[k] = b[k];
             }
-            getTime(stamp, sizeof(stamp));
+            manager::getTime(stamp, sizeof(stamp));
             out << stamp << "[Router: " << mypid << "] sending: " << buf << endl;
             if (send(sok, buf, sizeof(buf), 0) == -1) {
                 perror("[Router: ] send failed");
@@ -249,22 +271,22 @@ int router::startRouter(ofstream &ostr) {
             for (int k = 0; k < b.length(); k++) {
                 buf[k] = b[k];
             }
-            getTime(stamp, sizeof(stamp));
+            manager::getTime(stamp, sizeof(stamp));
             out << stamp << "[Router: " << mypid << "] sending: " << buf << endl;
             if (send(sok, buf, sizeof(buf), 0) == -1) {
                 perror("[Router: ] send failed");
                 exit(6);
             }
-            sleep(1);
+            //sleep(1);
             return 0;
         } else {
             if (sendit) {
                 memset(&buf, 0, sizeof(buf));
-                for (int k = 0; k < portnum.length(); k++) {
-                    buf[k] = portnum[k];
+                for (int k = 0; k < port.length(); k++) {
+                    buf[k] = port[k];
                 }
                 if (debug) {
-                    getTime(stamp, sizeof(stamp));
+                    manager::getTime(stamp, sizeof(stamp));
                     out << stamp << "[Router: " << mypid << "] sending: " << buf << endl;
                 }
                 if (send(sok, buf, sizeof(buf), 0) == -1) {
@@ -280,7 +302,7 @@ int router::startRouter(ofstream &ostr) {
                     if (nbytes == 0) {
                         // connection closed
                         if (debug) {
-                            getTime(stamp, sizeof(stamp));
+                            manager::getTime(stamp, sizeof(stamp));
                             cerr << "[Router: " << mypid << "] socket disconnected. Buf: " << buf << endl;
                         }
                         return 0;
@@ -289,16 +311,24 @@ int router::startRouter(ofstream &ostr) {
                         exit(6);
                     }
                 } else {
-                    getTime(stamp, sizeof(stamp));
+                    manager::getTime(stamp, sizeof(stamp));
                     out << stamp << "[Router: " << mypid << "] recv: " << buf << endl;
                     packet = buf;
                     in = packet.substr(0, 4);
-                    id = packet.substr(4, 5);
+                    id = packet.substr(5, 1);
                     if (in == "INFO") {
-                        /*TODO actually do the get ready stuff when info comes*/
                         ready = true;
                         myID = id;
-                        cout << "[Router: " << mypid << "] is id: " << id << endl;
+                        adj = packet.substr(6);
+
+                        if (adj.length() >= 1) {
+                            parseLinks(adj);
+                        } else {
+                            cerr << "[Router: " << mypid << "] has no neighbors! Closing node. " << id << endl;
+                            return -1;
+                        }
+                        out << endl << "[Router: " << mypid << "] my id is: " << id << endl;
+                        out << "[Router: " << mypid << "] my adj links are: " << adj << endl << endl;
                     } else if (in == "AKRD") {
                         /*TODO actually do the LB stuff when info comes*/
                         ready = true;
@@ -312,7 +342,6 @@ int router::startRouter(ofstream &ostr) {
                         done = true;
                         cout << "[Router: " << mypid << "] is pretending to send messages... " << endl;
                     } else if (in == "QUIT") {
-                        /*TODO actually do the Quit stuff when info comes*/
                         quit = true;
                         cout << "[Router: " << mypid << "] is quitting " << endl;
                     }
@@ -320,4 +349,43 @@ int router::startRouter(ofstream &ostr) {
             }
         }
     }
+}
+
+void router::parseLinks(string s) {
+    manager::link temp;
+    string toss;
+    int sN = 0;
+    int dN = 0;
+    int co = 0;
+    int po = 0;
+    istringstream ss(s);
+
+    while (!ss.eof()) {
+        ss >> toss;
+        if (!ss >> sN) {
+            cerr << "went bad on sN" << endl;
+            exit(1);
+        }
+        if (!ss >> dN) {
+            cerr << "went bad on dN" << endl;
+            exit(1);
+        }
+        if (!ss >> co) {
+            cerr << "went bad on co" << endl;
+            exit(1);
+        }
+        if (!ss >> po) {
+            cerr << "went bad on po" << endl;
+            exit(1);
+        }
+        temp.sourceID = sN;
+        temp.destID = dN;
+        temp.cost = co;
+        temp.dPort = po;
+        if (nLinks >= links.size()) {
+            links.resize(links.size() * 2);
+        }
+        links.at(nLinks++) = temp;
+    }
+    links.resize(nLinks);
 }
